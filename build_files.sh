31db70e14a9ad678ea83c4c730ef25f8a72de861
Italow <devitalow@gmail.com>
2025-03-05 15:47:49 -0300

    atualizacao vercel4

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,11 @@ pip install -r requirements.txt
 # Coleta arquivos estáticos
 python manage.py collectstatic --noinput
 
-# Aplica as migrações (opcional, pode ser feito depois)
-python manage.py migrate --noinput 
+# Gera as migrações (caso tenha alguma pendente)
+python manage.py makemigrations
+
+# Aplica as migrações
+python manage.py migrate --noinput
+
+# Cria um superusuário se não existir
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin123') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell 

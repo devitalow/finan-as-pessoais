fcab0a07d89cf0a3f0196161a44af6b9235fc868
Italow <devitalow@gmail.com>
2025-03-05 15:16:19 -0300

    atualizacao vercel2

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
+# Instala as dependências
 pip install -r requirements.txt
-python manage.py collectstatic --noinput 
+
+# Coleta arquivos estáticos
+python manage.py collectstatic --noinput
+
+# Aplica as migrações (opcional, pode ser feito depois)
+python manage.py migrate --noinput 
